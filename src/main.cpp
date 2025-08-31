c7f3105ef54eee4420cbb476b3491c3b6e8d27a1
pablo10diez2 <pablo.diez@opendeusto.es>
2025-08-31 19:10:58 +0200

    first structs

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+struct prePrepare{
+    int v;
+    int n;
+    size_t d;
+    bool bit;
+};
+
+struct prepare{
+    int v;
+    int n;
+    size_t d;
+
+};
+
+struct 
+
 int main(){
     std::cout<<"Hello!"<<std::endl;
-
+    
     return 0;
 }
