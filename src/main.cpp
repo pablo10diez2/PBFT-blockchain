8cd957513849928875e129eb94e69b71fc1ffd6a
pablo10diez2 <pablo.diez@opendeusto.es>
2025-09-28 20:05:06 +0200

    pre-prepare and prepare structs

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
 #include <cassert>
+#include <vector>
 
 enum class MessageType{
-    REQUEST
+    REQUEST, REPLY, PREPREPARE, PREPARE
 };
 
 struct Request{
     enum MessageType messageType = MessageType::REQUEST;
     bool operation;
-    unsigned int timestamp;
-    unsigned short clientId;
+    unsigned int requestTimestamp;
+    unsigned int clientId;
+};
+
+struct Reply{
+  enum MessageType messageType = MessageType::REPLY;
+  int viewNumber;
+  unsigned int requestTimestamp;
+  unsigned int clientId;
+  unsigned int nodeId;
+  bool result;
+};
+
+struct PrePrepare{
+     enum MessageType messageType = MessageType::PREPREPARE;
+     unsigned int viewNumber;
+     unsigned int seqNumber;
+     size_t digest;
+}:
+
+struct Prepare{
+    enum MessageType messageType = MessageType::PREPARE;
+    unsigned int viewNumber;
+    unsigned int seqNumber;
+    size_t digest;
+    unsigned int nodeId;
+}
+
+struct Node{
+    bool isPrimary;
+    unsigned int nodeId;
+    std::vector<>
 };
 
 int main(){
-    Request r1 = {};
 
     return 0;
 }
