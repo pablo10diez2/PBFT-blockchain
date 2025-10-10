7909b1256a646746b7b1186b43f7525dbed1ed38
pablo10diez2 <pablo.diez@opendeusto.es>
2025-10-10 16:33:47 +0200

    restart

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,16 @@
 #include <iostream>
-#include <cassert>
-#include <vector>
+#include <thread>
 
-enum class MessageType{
-    REQUEST, REPLY, PREPREPARE, PREPARE
-};
-
-struct Request{
-    enum MessageType messageType = MessageType::REQUEST;
-    bool operation;
-    unsigned int requestTimestamp;
-    unsigned int clientId;
-};
-
-struct Reply{
-  enum MessageType messageType = MessageType::REPLY;
-  int viewNumber;
-  unsigned int requestTimestamp;
-  unsigned int clientId;
-  unsigned int nodeId;
-  bool result;
-};
-
-struct PrePrepare{
-     enum MessageType messageType = MessageType::PREPREPARE;
-     unsigned int viewNumber;
-     unsigned int seqNumber;
-     size_t digest;
-}:
-
-struct Prepare{
-    enum MessageType messageType = MessageType::PREPARE;
-    unsigned int viewNumber;
-    unsigned int seqNumber;
-    size_t digest;
-    unsigned int nodeId;
+void print(){
+    std::cout<<"aa"<<std::endl;
 }
 
-struct Node{
-    bool isPrimary;
-    unsigned int nodeId;
-    std::vector<>
-};
-
 int main(){
+    std::thread server (print);
+    std::thread client (print);
+
+    server.join();
+    client.join();
 
     return 0;
 }
